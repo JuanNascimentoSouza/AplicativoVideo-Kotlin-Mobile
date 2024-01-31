5348db04572fa6e38b0493026e5d52292d295e18
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2024-01-31 01:04:02 -0300

    BottomNavigationView

--- Aplicacao/app/src/main/java/com/example/aplicacao/MainActivity.kt
+++ Aplicacao/app/src/main/java/com/example/aplicacao/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.aplicacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aplicacao.databinding.ActivityMainBinding
+import com.example.aplicacao.util.UiUtil
+
+lateinit var binding : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavBar.setOnItemSelectedListener{menuItem->
+            when(menuItem.itemId) {
+                R.id.bottom_menu_home->{
+                    UiUtil.showToast(this,"Add video")
+                }
+                R.id.bottom_menu_add_video->{
+                    UiUtil.showToast(this,"Add video")
+                }
+                R.id.bottom_menu_profile->{
+                    UiUtil.showToast(this,"Add video")
+                }
+            }
+            false
+        }
+
     }
 }
